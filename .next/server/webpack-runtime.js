4fcc88cbcde0d82b954c648f1a9b84353c0fc3ec
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-05-19 17:36:22 -0700

    Remove Netlify config and deps

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("044d42bdb2f9a875")
+/******/ 		__webpack_require__.h = () => ("7f94a68f432412bb")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
