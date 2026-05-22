b528aaa0b2813596739ee0655f6bdbd5f4705497
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-05-22 15:51:13 -0700

    Updated tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -131,7 +131,7 @@ const config: Config = {
         },
       },
       animation: {
-        marquee: "marquee 30s linear infinite",
+        marquee: "marquee 15s linear infinite",
       },
       zIndex: {
         base: String(tokens["z-index"].base.value),
