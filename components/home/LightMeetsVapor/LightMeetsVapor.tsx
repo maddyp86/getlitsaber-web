bc5835b103a57136890fdce7f93724e9757894a4
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-05-23 11:39:32 -0700

    Updated LightMeetsVapor.tsx

--- components/home/LightMeetsVapor/LightMeetsVapor.tsx
+++ components/home/LightMeetsVapor/LightMeetsVapor.tsx
@@ -73,7 +73,7 @@ export default function LightMeetsVapor() {
               animate={visible ? { opacity: 1, y: 0 } : { opacity: 0, y: 28 }}
               transition={{ duration: 0.8, ease: EASE }}
             >
-              WHERE LIGHT<br />AND VAPOR MEET
+              WHERE LIGHT AND VAPOR MEET
             </motion.h2>
           </div>
 
