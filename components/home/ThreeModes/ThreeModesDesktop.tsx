83c753ca9e397c597be6b8969dea97595577f389
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-05-22 22:03:19 -0700

    Updated ThreeModesDesktop.tsx

--- components/home/ThreeModes/ThreeModesDesktop.tsx
+++ components/home/ThreeModes/ThreeModesDesktop.tsx
@@ -201,7 +201,7 @@ export default function ThreeModesDesktop({ className }: ThreeModesDesktopProps)
           <div style={{ display: "flex", width: "100%", gap: "50px", alignItems: "center", justifyContent: "center" }}>
 
             {/* Left — mode cards */}
-            <div style={{ display: "flex", alignShelf: "stretch", gap: "50px"}}>
+            <div style={{ display: "flex", flexDirection: "column", gap: "20px", width: "614px" }}>
 
               {/* Litsaber Mode card */}
               <div
