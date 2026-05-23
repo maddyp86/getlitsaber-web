e264a69e6ca6f9e5a6917c3ff35bdec7ad1da46c
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-05-23 09:50:41 -0700

    Updated ThreeModesDesktop.tsx

--- components/home/ThreeModes/ThreeModesDesktop.tsx
+++ components/home/ThreeModes/ThreeModesDesktop.tsx
@@ -398,7 +398,7 @@ export default function ThreeModesDesktop({ className }: ThreeModesDesktopProps)
   {/* Right — mode image */}
   <div
     style={{
-      width: "600px",
+      width: "517px",
       height: "auto",
       flex: "1 0 0",
       alignSelf: "stretch",
