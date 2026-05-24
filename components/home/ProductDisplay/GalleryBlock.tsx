cadbb6e6c3409062491332851485a182b8ca4dbc
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-05-24 11:48:20 -0700

    Updated GalleryBlock.tsx

--- components/home/ProductDisplay/GalleryBlock.tsx
+++ components/home/ProductDisplay/GalleryBlock.tsx
@@ -38,7 +38,7 @@ export default function GalleryBlock() {
     src={mainImage.src}
     alt={mainImage.alt}
     fill
-    className="object-cover"
+    className="object-contain"
     sizes="(min-width: 1024px) 500px, 100vw"
     priority
   />
