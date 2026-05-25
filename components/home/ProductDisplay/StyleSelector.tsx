ec3cb99ae1e191e3d1b8a7748c1e72d9eb92194a
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-05-25 10:33:18 -0700

    Updated StyleSelector.tsx

--- components/home/ProductDisplay/StyleSelector.tsx
+++ components/home/ProductDisplay/StyleSelector.tsx
@@ -15,7 +15,7 @@ export default function StyleSelector({ activeStyle, onStyleChange }: StyleSelec
         Available Styles
       </p>
 
-      <div className="grid grid-cols-2 gap-2">
+      <div className="grid grid-cols-2 gap-2 flex items-start">
         {STYLE_OPTIONS.map((option) => (
           <button
             key={option.id}
