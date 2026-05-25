7140b79fb02653fe1e6c7981da06d5e087ed1ee6
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-05-24 22:20:02 -0700

    Updated StyleSelector.tsx

--- components/home/ProductDisplay/StyleSelector.tsx
+++ components/home/ProductDisplay/StyleSelector.tsx
@@ -26,7 +26,7 @@ export default function StyleSelector({ activeStyle, onStyleChange }: StyleSelec
             }`}
           >
             <div className="flex items-start gap-[10px] self-stretch">
-              <div className="relative w-12 h-12 flex-shrink-0">
+              <div className="relative w-10 h-10 flex-shrink-0">
                 <Image
                   src={option.swatchSrc}
                   alt={option.swatchAlt}
