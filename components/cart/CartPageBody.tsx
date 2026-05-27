6f32716038a39a2908241d8cc87bc49131550198
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-05-27 14:10:17 -0700

    Updated CartPageBody.tsx

--- components/cart/CartPageBody.tsx
+++ components/cart/CartPageBody.tsx
@@ -99,8 +99,8 @@ export default function CartPageBody() {
                         <Image
                           src={line.image}
                           alt={line.title}
-                          width={72}
-                          height={72}
+                          width={100}
+                          height={100}
                           className="w-full h-full object-cover"
                         />
                       </div>
