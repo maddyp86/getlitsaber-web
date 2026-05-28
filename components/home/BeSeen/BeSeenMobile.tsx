9780e22d524dc5be3984b7b40499a1f72314a94c
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-05-28 15:26:48 -0700

    Updated BeSeenMobile.tsx

--- components/home/BeSeen/BeSeenMobile.tsx
+++ components/home/BeSeen/BeSeenMobile.tsx
@@ -42,9 +42,9 @@ export default function BeSeenMobile({ className }: BeSeenMobileProps) {
             />
             <div
               className="absolute inset-x-0 bottom-0 flex flex-col justify-between px-[20px] pb-[40px]"
-              style={{ height: "600px" }}
+              style={{ height: "550px" }}
             >
-              <div className="flex flex-col gap-[20px]" style={{ width: "327px", height: "350px" }}>
+              <div className="flex flex-col gap-[20px]" style={{ width: "auto", height: "350px" }}>
                 <p className="font-label text-eyebrow text-accent-cyan tracking-widest uppercase">
                   {stage.eyebrow}
                 </p>
