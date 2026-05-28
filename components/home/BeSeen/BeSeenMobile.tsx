732837ffb73aa0dd639fe1faed57ec4a0e78530f
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-05-28 15:26:59 -0700

    Updated BeSeenMobile.tsx

--- components/home/BeSeen/BeSeenMobile.tsx
+++ components/home/BeSeen/BeSeenMobile.tsx
@@ -41,7 +41,7 @@ export default function BeSeenMobile({ className }: BeSeenMobileProps) {
               aria-hidden="true"
             />
             <div
-              className="absolute inset-x-0 bottom-0 flex flex-col justify-between px-[20px] pb-[40px]"
+              className="absolute inset-x-0 bottom-0 flex flex-col justify-between px-[20px] pb-[50px]"
               style={{ height: "550px" }}
             >
               <div className="flex flex-col gap-[20px]" style={{ width: "auto", height: "350px" }}>
