48541aa9098e00cbeef0cd8f2d2bcbf65f30c255
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-06-02 16:52:24 -0700

    Updated DescriptionSection.tsx

--- components/product/ProductDisplay/DescriptionSection.tsx
+++ components/product/ProductDisplay/DescriptionSection.tsx
@@ -3,7 +3,7 @@ import { DESCRIPTION_HEADING, DESCRIPTION_BODY } from "./productdisplay.content"
 export default function DescriptionSection() {
   return (
     <div className="flex flex-col items-start gap-5">
-      <h3 className="font-body text-[35px] font-bold text-white leading-normal">
+      <h3 className="font-body text-[30px] font-bold text-white leading-normal">
         {DESCRIPTION_HEADING}
       </h3>
       <p className="font-body text-[16px] font-normal text-[#CCC] leading-normal">
