7a0180b53f22bf4021d8a5df31f2bd09555fca28
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-06-02 16:52:17 -0700

    Updated DescriptionSection.tsx

--- components/product/ProductDisplay/DescriptionSection.tsx
+++ components/product/ProductDisplay/DescriptionSection.tsx
@@ -6,7 +6,7 @@ export default function DescriptionSection() {
       <h3 className="font-body text-[35px] font-bold text-white leading-normal">
         {DESCRIPTION_HEADING}
       </h3>
-      <p className="font-body text-[20px] font-normal text-[#CCC] leading-normal">
+      <p className="font-body text-[16px] font-normal text-[#CCC] leading-normal">
         {DESCRIPTION_BODY}
       </p>
     </div>
