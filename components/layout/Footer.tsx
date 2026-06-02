f507f4e9289f3eaebbab7c92b8e3338f8d3456d4
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-06-02 15:10:09 -0700

    Updated Footer.tsx

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -23,7 +23,7 @@ export default function Footer() {
     <footer className="bg-background-primary border-t border-surface-tint-white"
       style={{ background: "linear-gradient(180deg, #150C2D 0%, #000 100%)" }}
       >
-     <div className="mx-auto max-w-container px-container-mobile lg:px-container py-8 lg:py-12">
+     <div className="mx-auto max-w-container px-container-mobile lg:px-container py-10 lg:py-12">
         {/* Top section */}
         <div className="flex flex-col lg:flex-row lg:justify-between gap-10">
           {/* Brand column */}
