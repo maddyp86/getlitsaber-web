ee4ef4034f517c9d25e4f30aea945728d483f21a
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-06-02 16:34:01 -0700

    Updated page.tsx

--- app/shop/litsaber-og/page.tsx
+++ app/shop/litsaber-og/page.tsx
@@ -23,7 +23,7 @@ export default async function PDPPage() {
   return (
     <div className="pt-navbar px-container-mobile lg:px-container-desktop py-xl">
       <PdpViewTracker />
-      <div className="mx-100 w-full max-w-[1250px]">
+      <div className="ml-100 w-full max-w-[1250px]">
         <ProductDisplay
           variantId={silverVariant?.id ?? ""}
           available={available}
