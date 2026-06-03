2bc32f2a5795f2d8e0b278991fc7bf28340f41af
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-06-02 20:16:28 -0700

    Updated WholesaleCTABanner.tsx

--- components/home/WholesaleCTABanner/WholesaleCTABanner.tsx
+++ components/home/WholesaleCTABanner/WholesaleCTABanner.tsx
@@ -22,7 +22,7 @@ export default function WholesaleCTABanner() {
             style={{
               fontSize: "clamp(22px, 3.2vw, 35px)",
               fontStyle: "normal",
-              lineHeight:"50px"
+              lineHeight:"20px"
             }}
           >
             STOCK LITSABER IN YOUR{" "}
