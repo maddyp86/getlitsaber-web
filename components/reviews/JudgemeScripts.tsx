4bac6cbf0387647a4056adff7c672ed8644ac372
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-06-02 21:12:56 -0700

    Updated JudgemeScripts.tsx

--- components/reviews/JudgemeScripts.tsx
+++ components/reviews/JudgemeScripts.tsx
@@ -3,13 +3,13 @@ import Script from "next/script";
 export default function JudgemeScripts() {
   return (
     <>
-      <Script id="jdgm-config" strategy="afterInteractive">
+      <Script id="jdgm-config" strategy="beforeInteractive">
         {`jdgm = window.jdgm || {}; jdgm.SHOP_DOMAIN = 'ajur1e-s1.myshopify.com'; jdgm.PLATFORM = 'shopify'; jdgm.PUBLIC_TOKEN = 'gijUs8wKxVb6h7ZybFfXSiESlGY';`}
       </Script>
       <Script
         id="jdgm-preloader"
         src="https://cdnwidget.judge.me/widget_preloader.js"
-        strategy="afterInteractive"
+        strategy="beforeInteractive"
         data-cfasync="false"
       />
     </>
