6b2cf3ea91dad899bf86a8d577000ed0a708c62e
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-06-03 20:42:58 -0700

    Updated PowerSection.tsx

--- components/the-tech/PowerSection.tsx
+++ components/the-tech/PowerSection.tsx
@@ -23,7 +23,7 @@ export default function PowerSection() {
       className="relative w-full bg-background-primary"
       aria-label="Power that keeps up"
     >
-      <div className="mx-auto w-full max-w-[1250px] px-5 lg:px-0 py-16 lg:py-24">
+      <div className="mx-auto w-full max-w-[1250px] lg:px-[60px] px-[20px] py-[100px]">
 
         {/* 2-col: image left, copy right */}
         <div className="flex flex-col lg:flex-row lg:items-center lg:gap-16">
