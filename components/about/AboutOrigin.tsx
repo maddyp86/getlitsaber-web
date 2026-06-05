ea4ea833554dbdad2680e038f45b2f00179e0452
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-06-05 11:33:19 -0700

    Updated AboutOrigin.tsx

--- components/about/AboutOrigin.tsx
+++ components/about/AboutOrigin.tsx
@@ -73,7 +73,7 @@ export default function AboutOrigin() {
   </span>
 
   <p
-    className="text-center font-subhead leading-normal"
+    className="text-center font-subhead leading-[1.1]"
     style={{ fontSize: "clamp(26px, 4.5vw, 45px)" }}
   >
     <span className="font-normal text-white">{ORIGIN_PULLQUOTE} </span>
