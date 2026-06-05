2f8e0f6d61a328ce6d94f6a25ca68a502472e93b
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-06-04 21:27:24 -0700

    Updated RetailKit.tsx

--- components/wholesale/RetailKit.tsx
+++ components/wholesale/RetailKit.tsx
@@ -14,17 +14,6 @@ import {
 
 const EASE: [number, number, number, number] = [0.16, 1, 0.3, 1];
 
-function renderEmphasis(text: string) {
-  return text.split("**").map((segment, i) =>
-    i % 2 === 1 ? (
-      <strong key={i} className="font-bold text-white">
-        {segment}
-      </strong>
-    ) : (
-      segment
-    )
-  );
-}
 
 export default function RetailKit() {
   const prefersReduced = useReducedMotion();
