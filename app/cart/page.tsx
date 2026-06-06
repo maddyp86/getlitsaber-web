f0be29227740e8f67c6148e6d66c0a2d3f6f5071
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-06-05 22:47:34 -0700

    Fix cart page route

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import CartPageBody from "@/components/cart/CartPageBody";
 
 export const metadata: Metadata = {
-  title: "Cart",
+  title: "Cart | Litsaber",
 };
 
 export default function CartPage() {
