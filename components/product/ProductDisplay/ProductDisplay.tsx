b3d0ea9656279a9d7a97a6a66209130828738059
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-06-07 13:08:22 -0700

    Updated ProductDisplay.tsx

--- components/product/ProductDisplay/ProductDisplay.tsx
+++ components/product/ProductDisplay/ProductDisplay.tsx
@@ -39,18 +39,18 @@ export default function ProductDisplay({ variantId, available, surface }: Produc
   const displayPrice = `$${getTierPrice(selectedQty).toFixed(2)}`;
 
   return (
-<div className="w-full flex flex-col lg:flex-row items-start justify-center gap-6 lg:gap-[50px]">
+    <div className="w-full flex flex-col lg:flex-row items-start justify-center gap-6 lg:gap-[50px]">
       {/* Left: image gallery */}
-<div
-  className="flex flex-col items-start w-full lg:w-[700px] lg:flex-shrink-0 lg:sticky lg:self-start lg:top-[100px]"
-  style={{ gap: "20px" }}
->
+      <div
+        className="flex flex-col items-start w-full lg:w-[600px] lg:flex-shrink-0 lg:sticky lg:self-start lg:top-[100px]"
+        style={{ gap: "20px" }}
+      >
         <GalleryBlock activeThumb={activeThumb} onThumbClick={setActiveThumb} />
       </div>
 
       {/* Right: product info */}
       <div
-        className="w-full lg:w-[700px] lg:flex-shrink-0 min-w-0 flex flex-col items-stretch gap-6 lg:gap-[30px]"
+        className="w-full lg:w-[560px] lg:flex-shrink-0 min-w-0 flex flex-col items-stretch gap-6 lg:gap-[30px]"
       >
           {/* Title + subtitle + price */}
           <div className="flex flex-col gap-2">
