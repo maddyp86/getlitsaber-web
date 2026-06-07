2728626e182a1ecbc48c880a6c1099372a8188ac
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-06-07 13:04:06 -0700

    Updated ProductDisplay.tsx

--- components/product/ProductDisplay/ProductDisplay.tsx
+++ components/product/ProductDisplay/ProductDisplay.tsx
@@ -50,7 +50,7 @@ export default function ProductDisplay({ variantId, available, surface }: Produc
 
       {/* Right: product info */}
       <div
-        className="w-full lg:w-[700px] lg:flex-shrink-0 min-w-0 flex flex-col items-stretch gap-6 lg:gap-[40px]"
+        className="w-full lg:w-[700px] lg:flex-shrink-0 min-w-0 flex flex-col items-stretch gap-6 lg:gap-[30px]"
       >
           {/* Title + subtitle + price */}
           <div className="flex flex-col gap-2">
