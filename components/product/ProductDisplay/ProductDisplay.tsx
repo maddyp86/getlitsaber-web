439a01418cf19db87f041df978f43b397e80f646
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-06-07 12:56:56 -0700

    Updated ProductDisplay.tsx

--- components/product/ProductDisplay/ProductDisplay.tsx
+++ components/product/ProductDisplay/ProductDisplay.tsx
@@ -39,7 +39,7 @@ export default function ProductDisplay({ variantId, available, surface }: Produc
   const displayPrice = `$${getTierPrice(selectedQty).toFixed(2)}`;
 
   return (
-    <div className="px-5 lg:px-0 flex flex-col lg:flex-row items-start gap-6 lg:gap-[50px]">
+    <div className="w-full px-5 lg:px-0 flex flex-col lg:flex-row items-start gap-6 lg:gap-[50px]">
       {/* Left: image gallery */}
       <div
   className="flex flex-col items-start w-full lg:flex-1 lg:max-w-[640px] lg:sticky lg:self-start lg:top-[96px]"
@@ -50,7 +50,7 @@ export default function ProductDisplay({ variantId, available, surface }: Produc
 
       {/* Right: product info */}
       <div
-        className="w-full lg:w-[525px] lg:flex-shrink-0 min-w-0 flex flex-col items-stretch gap-6 lg:gap-[33px]"
+        className="w-full lg:w-[600px] lg:flex-shrink-0 min-w-0 flex flex-col items-stretch gap-6 lg:gap-[33px]"
       >
           {/* Title + subtitle + price */}
           <div className="flex flex-col gap-2">
