f0644da344970be5311a5d82fcf95bd3a6e7119a
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-06-06 19:18:14 -0700

    Updated HeroDesktop.tsx

--- components/home/HeroDesktop.tsx
+++ components/home/HeroDesktop.tsx
@@ -78,7 +78,7 @@ export default function HeroDesktop({ className }: HeroDesktopProps) {
           </motion.p>
 
           <motion.div
-            className="flex flex-row gap-md justify-center w-full max-w-[900px] mx-auto"
+            className="flex flex-row gap-md justify-center w-full"
             variants={variants}
             initial="hidden"
             animate="visible"
