5b6d9682969bfcc1931cc663bf51ebb9df3c13ad
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-06-09 14:59:59 -0700

    Updated ActivateCharging.tsx

--- components/activate/ActivateCharging.tsx
+++ components/activate/ActivateCharging.tsx
@@ -44,7 +44,7 @@ export default function ActivateCharging() {
 
         <h2
           className="font-display font-bold uppercase leading-[1.1] text-white mb-2"
-          style={{ fontSize: "clamp(45px, 6vw, 75px)" }}
+          style={{ fontSize: "clamp(45px, 6vw, 75px)", textShadow: "0 0 50px rgba(255,255,255,0.50)" }}
         >
           {title}
         </h2>
