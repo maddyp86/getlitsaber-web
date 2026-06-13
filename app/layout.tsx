97bbfcd7b555a30fd8bee2750d8fa7ce0dc316c3
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-06-13 14:51:33 -0700

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,9 +22,9 @@ export const metadata: Metadata = {
     template: "%s | Litsaber",
   },
   icons: {
-    icon: "https://0ku6zb3bovdlowuq.public.blob.vercel-storage.com/images/global/litsaber-icon-black.png",
-    shortcut: "https://0ku6zb3bovdlowuq.public.blob.vercel-storage.com/images/global/litsaber-icon-black.png",
-    apple: "https://0ku6zb3bovdlowuq.public.blob.vercel-storage.com/images/global/litsaber-icon-black.png",
+    icon: "https://0ku6zb3bovdlowuq.public.blob.vercel-storage.com/images/global/litsaber-icon-white.png",
+    shortcut: "https://0ku6zb3bovdlowuq.public.blob.vercel-storage.com/images/global/litsaber-icon-white.png",
+    apple: "https://0ku6zb3bovdlowuq.public.blob.vercel-storage.com/images/global/litsaber-icon-white.png",
   },
   description:
     "A glowstick that hits 510 carts. Built for festivals, nightlife, and the moments worth being lit for.",
