aa5cd105722b93051e03820fd24fe782d0831805
Matthew <94876913+maddyp86@users.noreply.github.com>
2026-06-13 15:01:38 -0700

    Updated ProductDisplay.tsx

--- components/product/ProductDisplay.tsx
+++ components/product/ProductDisplay.tsx
@@ -39,10 +39,10 @@ export default function ProductDisplay({ variantId, available, surface }: Produc
   const displayPrice = `$${getTierPrice(selectedQty).toFixed(2)}`;
 
   return (
-    <div className="w-full flex flex-col lg:flex-row items-start justify-center gap-6 lg:gap-[50px]">
+    <div className="w-full flex flex-col lg:flex-row items-start justify-center gap-6 lg:gap-[30px]">
       {/* Left: image gallery */}
       <div
-        className="flex flex-col items-start w-full lg:w-[500px] lg:max-w-[500px] lg:flex-shrink-0 min-w-0 lg:sticky lg:self-start lg:top-[50px]"
+        className="flex flex-col items-start w-full lg:w-[525px] lg:max-w-[525px] lg:flex-shrink-0 min-w-0 lg:sticky lg:self-start lg:top-[30px]"
         style={{ gap: "20px" }}
       >
         <GalleryBlock activeThumb={activeThumb} onThumbClick={setActiveThumb} />
